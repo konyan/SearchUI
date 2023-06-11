7832ae37d999e4c13cc708aaf25bb3ed83868d84
konyan <devkonyan@gmail.com>
2023-06-11 18:12:29 +0700

    fix jest setup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,27 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@assets': './assets',
+            '@components': './src/components',
+            '@hooks': './src/hooks',
+            '@screens': './src/screens',
+          },
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
   };
 };

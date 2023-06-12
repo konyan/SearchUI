71c298720e16b6c81f6004f2ba9c891fa554ba39
konyan <devkonyan@gmail.com>
2023-06-12 22:46:51 +0700

    jest test hooks

---
+++ src/screens/HomeScreen/hooks/__test__/useHomeScreenHook.test.tsx
@@ -0,0 +1,43 @@
+import { act, renderHook } from '@testing-library/react-native';
+import { useStore } from '../../../../core/zustandStore';
+import useHomeScreenHook from '../useHomeScreenHook';
+
+jest.mock('../../../../core/zustandStore', () => {
+  return {
+    useStore: jest.fn(),
+  };
+});
+
+describe('useHomeScreenHook', () => {
+  const mockUseStore = useStore as jest.MockedFunction<typeof useStore>;
+
+  beforeEach(() => {
+    mockUseStore.mockClear();
+    mockUseStore.mockReturnValue({
+      error: false,
+      loading: false,
+      data: [],
+      searchByUserName: jest.fn(),
+      closeError: jest.fn(),
+    });
+  });
+
+  it('should return correct data and default value', () => {
+    const { result } = renderHook(() => useHomeScreenHook());
+
+    expect(result.current.error).toBe(false);
+    expect(result.current.loading).toBe(false);
+    expect(result.current.result).toEqual([]);
+    expect(result.current.searchText).toBe('');
+  });
+
+  it('should call onSearchName, after that searchText will change', async () => {
+    const { result } = renderHook(() => useHomeScreenHook());
+    result.current.onSearchName('hello');
+
+    act(() => {
+      result.current.searchText = 'hello';
+    });
+    expect(result.current.searchText).toBe('hello');
+  });
+});

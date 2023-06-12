5fe48fbaceff6e60389c184f41dc5c04fcd0777d
konyan <devkonyan@gmail.com>
2023-06-12 15:34:57 +0700

    add zustand logic and update

---
+++ src/core/zustandStore.ts
@@ -0,0 +1,49 @@
+/* eslint-disable max-nested-callbacks */
+import { Data } from 'src/hooks/useDataHook';
+import { create } from 'zustand';
+import JsonData from '../../data/leaderboard.json';
+
+const getInitData = (): Data[] => {
+  const jsonArray: { [key: string]: Data } = JsonData;
+  const array = Object.keys(jsonArray).map((key) => jsonArray[key]);
+  return array;
+};
+
+interface IState {
+  data: Data[];
+  loading: boolean;
+  searchByUserName: (userName: string) => void;
+}
+
+export const useStore = create<IState>((set, get) => ({
+  data: [],
+  searchByUserName: (userName: string) => {
+    set({ loading: true });
+    setTimeout(() => {
+      const data = getInitData();
+      const sortUser = data
+        .sort((a, b) => {
+          return b.bananas - a.bananas;
+        })
+        .map((item, index) => {
+          return {
+            ...item,
+            rank: index + 1,
+          };
+        });
+
+      const filteredResult = sortUser.filter((item, index) => item.name.toLowerCase().includes(userName.toLowerCase()));
+      if (filteredResult.length > 0) {
+        const resultUser = filteredResult[0];
+        if (resultUser?.rank <= 10) {
+          const updateUser = sortUser.slice(0, 10);
+          set({ data: updateUser, loading: false });
+        } else {
+          const updateUser = sortUser.slice(0, 9).concat(resultUser);
+          set({ data: updateUser, loading: false });
+        }
+      }
+    }, 2000);
+  },
+  loading: false,
+}));
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,42 +1,24 @@
 import { ListItem, SearchBar } from '@components';
-import { tw } from '@core';
-import { useDataHook } from '@hooks';
+import { tw, useStore } from '@core';
 import React, { useState } from 'react';
-import { FlatList, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import { Data } from 'src/hooks/useDataHook';
 
 const HomeScreen = () => {
   const [searchText, setSearchText] = useState<string>('');
 
-  const { data: LeaderBoardData } = useDataHook();
+  const result = useStore((state) => state.data);
 
-  const [result, setResult] = useState<Data[]>();
+  const loading = useStore((state) => state.loading);
+
+  const searchByUserName = useStore((state) => state.searchByUserName);
 
   const onSearchName = (text: string) => {
     setSearchText(text);
   };
 
   const onSearchUsername = () => {
-    const sortUser = LeaderBoardData.sort((a, b) => {
-      return b.bananas - a.bananas;
-    }).map((item, index) => {
-      return {
-        ...item,
-        rank: index + 1,
-      };
-    });
-
-    const filteredResult = sortUser.filter((item, index) => item.name.toLowerCase().includes(searchText.toLowerCase()));
-    if (filteredResult.length > 0) {
-      const resultUser = filteredResult[0];
-      if (resultUser?.rank <= 10) {
-        const updateUser = sortUser.slice(0, 10);
-        setResult(updateUser);
-      } else {
-        const updateUser = sortUser.slice(0, 9).concat(resultUser);
-        setResult(updateUser);
-      }
-    }
+    searchByUserName(searchText);
   };
 
   return (
@@ -47,6 +29,12 @@ const HomeScreen = () => {
         placeholder="Search username"
         onSearch={onSearchUsername}
       />
+
+      {loading && (
+        <View>
+          <Text>loading..</Text>
+        </View>
+      )}
       <FlatList
         style={tw`w-full mb-8`}
         scrollEnabled={true}
